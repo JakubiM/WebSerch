1af81d612cba075ad4728459e565acfd456c5b3d
JakubiM <jakubmalek888@gmail.com>
2019-04-28 19:34:09 +0200

    Miasto + temperatura

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,42 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.net.URL;
+
+
 public class Main {
 
-    public static void main(String[] args){
-        System.out.println("Hello");
-        System.out.println("Commit 2");
+    private static String Url = "https://pogoda.interia.pl/prognoza-dlugoterminowa-ciosmy,cId,4885";
+    private static int timeout = 20000;
 
-        System.out.println("PUSZ");
+    public static void main(String[] args){
+
+
+        Document document;
+        try
+        {
+            document = Jsoup.parse(new URL(Url), timeout);
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+            return;
+        }
+
+        Elements temperatura = document.select(".weather-currently-temp-strict");
+        assert (temperatura.size() == 1);
+        String aktualnaTemp = temperatura.first().text();
+
+        Elements miejscowosc = document.select(".weather-currently-city");
+        assert (temperatura.size() == 1);
+        String akutalnaMiejsc = miejscowosc.first().text();
+
+
+
+
+
+        System.out.println("Aktualna temperatura w " + akutalnaMiejsc +" to: " + aktualnaTemp);
     }
 }
